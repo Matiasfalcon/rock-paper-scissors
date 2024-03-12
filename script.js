7bbf80c5f2567a0b7d0c6669a52e9e15cae36464
Matias <matias.e.falcon@gmail.com>
2024-03-12 00:22:19 -0300

    add functionality, user interface and the final logic

--- script.js
+++ script.js
@@ -1,52 +1,95 @@
+let btnRock = document.querySelector(".btn-rock");
+let btnPaper = document.querySelector(".btn-paper");
+let btnScissor = document.querySelector(".btn-scissor");
+let selectionInGame = document.querySelector(".selection-in-game");
+let btnReset = document.querySelector(".reset");
+let pScore = document.querySelector(".p-score");
+let cScore = document.querySelector(".c-score");
+let pStartScore = 1;
+let cStartScore = 1;
+
+btnRock.addEventListener("click", handleClick);
+btnPaper.addEventListener("click", handleClick);
+btnScissor.addEventListener("click", handleClick);
+btnReset.addEventListener("click", resetGame);
+
+// function to reset game
+function resetGame() {
+    pStartScore = 1;
+    cStartScore = 1;
+    cScore.innerHTML = 0;
+    pScore.innerHTML = 0;
+    selectionInGame.innerHTML = "Select your weapon!";
+}
+
+//function to hadle click event
+function handleClick() {
+    playRound(this.id);
+}
+
 // function for computer choice
 function getComputerChoice() {
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["Rock", "Paper", "Scissor"];
     let random = Math.floor(Math.random() * 3);
     return options[random];
-    }
-    
-    // function that plays a single round
-    function playRound(playerSelection, computerSelection) {
-    // convert to lower case
-    let pSelection = playerSelection.toLowerCase();
-    let cSelection = computerSelection.toLowerCase();
+}
+
+// function that plays a single round
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
     // checking comparison
-    if (pSelection == "rock" & cSelection == "scissors") {
-    return "You win! Rock beats Scissors.";
-    } if (pSelection == "paper" & cSelection == "rock") {
-    return "You win! Paper beats Rock.";
-    } if (pSelection == "scissors" & cSelection == "paper") {
-    return "You win! Scissors beats paper.";
-    } if (pSelection == cSelection) {
-    return "It's a draw!";
+    if (playerSelection == computerSelection) {
+        selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`;
+        pScore.innerHTML = pStartScore++;
+        cScore.innerHTML = cStartScore++;
+        return console.log("Draw")
+    } else {
+        if (playerSelection == "Paper" & computerSelection == "Scissor") {
+            selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`;
+            cScore.innerHTML = cStartScore++;
+            return console.log("Lose")
+        } else {
+            if (playerSelection == "Rock" & computerSelection == "Paper") {
+                selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`
+                cScore.innerHTML = cStartScore++;
+                return console.log("Lose")
+            } else {
+                if (playerSelection == "Scissor" & computerSelection == "Rock") {
+                    selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`
+                    cScore.innerHTML = cStartScore++;
+                    return console.log("Lose")
+                } else {
+                    selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`
+                    pScore.innerHTML = pStartScore++;
+                    return console.log("Win")
+                }
+            }
+        }
     }
-    else {
-    return `You Lose! ${computerSelection} beats ${playerSelection}.`;
-    }
-    }
-    
-    // function to play entire game
-    function playGame() {
+}
+
+// function to play entire game
+function playGame() {
     console.log("Welcome! let's Play Rock, Paper and Scissors game!");
     let playerScore = 0;
     let computerScore = 0;
     // five round loop
     for (let i = 0; i < 5; i++) {
-    let pSelection = prompt("select option(Rock, Scissors or Paper)");
-    let cSelection = getComputerChoice();;
-    let result = playRound(pSelection, cSelection);
-    // checking if player win or loose
-    if (result[4] == 'w') {
-    playerScore++;
-    } else {
-    if (result[4] == 'l') {
-    computerScore++;
-    } else {
-    computerScore++;
-    playerScore++;
-    }
-    }
-    console.log(result);
+        let pSelection = prompt("select option(Rock, Scissors or Paper)");
+        let cSelection = getComputerChoice();;
+        let result = playRound(pSelection, cSelection);
+        // checking if player win or loose
+        if (result[4] == 'w') {
+            playerScore++;
+        } else {
+            if (result[4] == 'l') {
+                computerScore++;
+            } else {
+                computerScore++;
+                playerScore++;
+            }
+        }
+        console.log(result);
     }
     console.log(`Final Score: Player = ${playerScore} Computer = ${computerScore}`);
-    }
+}
