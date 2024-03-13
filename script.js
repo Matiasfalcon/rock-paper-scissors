7624852da7d2ac6cef84643e690aec18edc65da5
Matias <matias.e.falcon@gmail.com>
2024-03-13 16:40:41 -0300

    applying good practices and some modifications to the already finished and functional code

--- script.js
+++ script.js
@@ -1,95 +1,73 @@
-let btnRock = document.querySelector(".btn-rock");
-let btnPaper = document.querySelector(".btn-paper");
-let btnScissor = document.querySelector(".btn-scissor");
-let selectionInGame = document.querySelector(".selection-in-game");
-let btnReset = document.querySelector(".reset");
-let pScore = document.querySelector(".p-score");
-let cScore = document.querySelector(".c-score");
-let pStartScore = 1;
-let cStartScore = 1;
+// Get elements from the DOM
+const btnRock = document.querySelector(".btn-rock");
+const btnPaper = document.querySelector(".btn-paper");
+const btnScissor = document.querySelector(".btn-scissor");
+const selectionMessage = document.querySelector(".selection-message");
+const btnReset = document.querySelector(".reset-game");
+const playerScoreElement = document.querySelector(".player-score");
+const computerScoreElement = document.querySelector(".computer-score");
 
-btnRock.addEventListener("click", handleClick);
-btnPaper.addEventListener("click", handleClick);
-btnScissor.addEventListener("click", handleClick);
+// Initial scores
+let playerScore = 0;
+let computerScore = 0;
+
+// Adding event listeners
+btnRock.addEventListener("click", () => handleButtonClick("Rock"));
+btnPaper.addEventListener("click", () => handleButtonClick("Paper"));
+btnScissor.addEventListener("click", () => handleButtonClick("Scissor"));
 btnReset.addEventListener("click", resetGame);
 
-// function to reset game
+// Game restart function
 function resetGame() {
-    pStartScore = 1;
-    cStartScore = 1;
-    cScore.innerHTML = 0;
-    pScore.innerHTML = 0;
-    selectionInGame.innerHTML = "Select your weapon!";
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
+    updateSelectionMessage("Select your weapon!");
 }
 
-//function to hadle click event
-function handleClick() {
-    playRound(this.id);
+// Function to handle the click of a button
+function handleButtonClick(playerSelection) {
+    playRound(playerSelection);
 }
 
-// function for computer choice
+// Computer selection function
 function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissor"];
-    let random = Math.floor(Math.random() * 3);
-    return options[random];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
 }
 
-// function that plays a single round
+// Function that plays a round
 function playRound(playerSelection) {
-    let computerSelection = getComputerChoice();
-    // checking comparison
-    if (playerSelection == computerSelection) {
-        selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`;
-        pScore.innerHTML = pStartScore++;
-        cScore.innerHTML = cStartScore++;
-        return console.log("Draw")
-    } else {
-        if (playerSelection == "Paper" & computerSelection == "Scissor") {
-            selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`;
-            cScore.innerHTML = cStartScore++;
-            return console.log("Lose")
-        } else {
-            if (playerSelection == "Rock" & computerSelection == "Paper") {
-                selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`
-                cScore.innerHTML = cStartScore++;
-                return console.log("Lose")
-            } else {
-                if (playerSelection == "Scissor" & computerSelection == "Rock") {
-                    selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`
-                    cScore.innerHTML = cStartScore++;
-                    return console.log("Lose")
-                } else {
-                    selectionInGame.innerHTML = `${playerSelection} vs ${computerSelection}`
-                    pScore.innerHTML = pStartScore++;
-                    return console.log("Win")
-                }
-            }
-        }
+    const computerSelection = getComputerChoice();
+    updateSelectionMessage(`${playerSelection} vs ${computerSelection}`);
+    const result = compareChoices(playerSelection, computerSelection);
+
+    if (result === "win") {
+        playerScore++;
+    } else if (result === "lose") {
+        computerScore++;
     }
+
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
 }
 
-// function to play entire game
-function playGame() {
-    console.log("Welcome! let's Play Rock, Paper and Scissors game!");
-    let playerScore = 0;
-    let computerScore = 0;
-    // five round loop
-    for (let i = 0; i < 5; i++) {
-        let pSelection = prompt("select option(Rock, Scissors or Paper)");
-        let cSelection = getComputerChoice();;
-        let result = playRound(pSelection, cSelection);
-        // checking if player win or loose
-        if (result[4] == 'w') {
-            playerScore++;
-        } else {
-            if (result[4] == 'l') {
-                computerScore++;
-            } else {
-                computerScore++;
-                playerScore++;
-            }
-        }
-        console.log(result);
-    }
-    console.log(`Final Score: Player = ${playerScore} Computer = ${computerScore}`);
+// Function to compare the choices of the player and the computer
+function compareChoices(playerSelection, computerSelection) {
+    const winMap = {
+        Rock: "Scissor",
+        Paper: "Rock",
+        Scissor: "Paper",
+    };
+
+    return winMap[playerSelection] === computerSelection ? "win" :
+        winMap[computerSelection] === playerSelection ? "lose" :
+            "draw";
+}
+
+// Function to update the selection message
+function updateSelectionMessage(message) {
+    selectionMessage.textContent = message;
 }
